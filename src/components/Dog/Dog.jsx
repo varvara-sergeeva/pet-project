653fa4d22c0088201b3dd061b67627c4f9254dbe
varvara-sergeeva <raunuperetemheru@gmail.com>
2023-05-12 00:50:07 +0300

    new get request via token

--- src/components/Dog/Dog.jsx
+++ src/components/Dog/Dog.jsx
@@ -1,19 +1,40 @@
 import './Dog.scss';
 
+//function Dog (props) {
+//	const {
+//		id,
+//		name,
+//		image: photo
+//	} = props;
+
+//	return (
+//		<div className="card dog">
+//			<div className="card__img">
+//				<img src={photo.url} alt={name} />
+//			</div>
+//			<div className="card__info">
+//				<h3 className="card__title">{name}</h3>
+//			</div>
+//		</div>
+//	)
+//}
+
 function Dog (props) {
 	const {
 		id,
 		name,
-		image: photo
+		age,
+		breeds,
 	} = props;
 
 	return (
 		<div className="card dog">
 			<div className="card__img">
-				<img src={photo.url} alt={name} />
 			</div>
 			<div className="card__info">
 				<h3 className="card__title">{name}</h3>
+				<p>{JSON.stringify(breeds.primary)}</p>
+				<span className="card__title">{age}</span>
 			</div>
 		</div>
 	)
