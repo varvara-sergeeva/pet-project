4cbb998a658cc64d5a8fe0f948d9d01100dec6e2
varvara-sergeeva <raunuperetemheru@gmail.com>
2023-05-29 01:39:27 +0300

    refactoring class component to func component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,102 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.scss";
 
-class Search extends React.Component {
-  state = {
-    search: "",
-    gender: "Male, Female",
-  };
+const Search = (props) => {
+  const {
+    //заглушка, функция пустышка если ничего не придет
+    searchDogs = Function.prototype,
+  } = props;
 
-  handelKey = (event) => {
+  const [search, setSearch] = useState("");
+  const [gender, setGender] = useState("Male, Female");
+
+  const handelKey = (event) => {
     if (event.key === "Enter") {
-      this.props.searchDogs(this.state.search, this.state.gender);
+      searchDogs(search, gender);
     }
   };
 
-  filterGender = (event) => {
-    this.setState(
-      () => ({ gender: event.target.dataset.gender }),
-      () => {
-        this.props.searchDogs(this.state.search, this.state.gender);
-      }
-    );
+  const filterGender = (event) => {
+    setGender(() => event.target.dataset.gender);
+    searchDogs(search, event.target.dataset.gender);
   };
 
-  render() {
-    return (
-      <div className="search">
-        <div className="search__body">
-          <input
-            placeholder="search breed"
-            type="search"
-            className="search__request"
-            value={this.state.search}
-            onChange={(e) => this.setState({ search: e.target.value })}
-            onKeyDown={this.handelKey}
-          />
-          <button
-            className="search__btn"
-            onClick={() =>
-              this.props.searchDogs(this.state.search, this.state.gender)
-            }
+  return (
+    <div className="search">
+      <div className="search__body">
+        <input
+          placeholder="search breed"
+          type="search"
+          className="search__request"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={handelKey}
+        />
+        <button
+          className="search__btn"
+          onClick={() => searchDogs(search, gender)}
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.4213 13.5999C11.2924 14.4774 9.87389 15 8.33329 15C4.65139 15 1.66663 12.0152 1.66663 8.33332C1.66663 4.65143 4.65139 1.66666 8.33329 1.66666C12.0152 1.66666 15 4.65143 15 8.33332C15 9.87392 14.4774 11.2925 13.5998 12.4214L18.0892 16.9107C18.4147 17.2362 18.4147 17.7638 18.0892 18.0892C17.7638 18.4147 17.2361 18.4147 16.9107 18.0892L12.4213 13.5999ZM13.3333 8.33332C13.3333 11.0947 11.0947 13.3333 8.33329 13.3333C5.57187 13.3333 3.33329 11.0947 3.33329 8.33332C3.33329 5.5719 5.57187 3.33332 8.33329 3.33332C11.0947 3.33332 13.3333 5.5719 13.3333 8.33332Z"
               fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M12.4213 13.5999C11.2924 14.4774 9.87389 15 8.33329 15C4.65139 15 1.66663 12.0152 1.66663 8.33332C1.66663 4.65143 4.65139 1.66666 8.33329 1.66666C12.0152 1.66666 15 4.65143 15 8.33332C15 9.87392 14.4774 11.2925 13.5998 12.4214L18.0892 16.9107C18.4147 17.2362 18.4147 17.7638 18.0892 18.0892C17.7638 18.4147 17.2361 18.4147 16.9107 18.0892L12.4213 13.5999ZM13.3333 8.33332C13.3333 11.0947 11.0947 13.3333 8.33329 13.3333C5.57187 13.3333 3.33329 11.0947 3.33329 8.33332C3.33329 5.5719 5.57187 3.33332 8.33329 3.33332C11.0947 3.33332 13.3333 5.5719 13.3333 8.33332Z"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="search__filter">
-          <div className="search__gender">
-            <label>
-              <input
-                name="gender"
-                type="radio"
-                className="search__radio"
-                data-gender="Male, Female"
-                onChange={this.filterGender}
-                checked={this.state.gender === "Male, Female"}
-              />
-              <span className="search__radio-box">All</span>
-            </label>
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="search__filter">
+        <div className="search__gender">
+          <label>
+            <input
+              name="gender"
+              type="radio"
+              className="search__radio"
+              data-gender="Male, Female"
+              onChange={filterGender}
+              checked={gender === "Male, Female"}
+            />
+            <span className="search__radio-box">All</span>
+          </label>
 
-            <label>
-              <input
-                name="gender"
-                type="radio"
-                className="search__radio"
-                data-gender="Male"
-                onChange={this.filterGender}
-                checked={this.state.gender === "Male"}
-              />
-              <span>Male</span>
-            </label>
-            <label>
-              <input
-                name="gender"
-                type="radio"
-                className="search__radio"
-                data-gender="Female"
-                onChange={this.filterGender}
-                checked={this.state.gender === "Female"}
-              />
-              <span>Female</span>
-            </label>
-          </div>
+          <label>
+            <input
+              name="gender"
+              type="radio"
+              className="search__radio"
+              data-gender="Male"
+              onChange={filterGender}
+              checked={gender === "Male"}
+            />
+            <span>Male</span>
+          </label>
+          <label>
+            <input
+              name="gender"
+              type="radio"
+              className="search__radio"
+              data-gender="Female"
+              onChange={filterGender}
+              checked={gender === "Female"}
+            />
+            <span>Female</span>
+          </label>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export { Search };
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import MainPageStyle from "./MainPage.module.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
@@ -7,50 +7,49 @@ import { Dogs } from "../../components/Dogs/Dogs";
 import { Search } from "../../components/Search/Search";
 import { Preloader } from "../../components/Preloader/Preloader";
 
-class MainPage extends React.Component {
-  state = {
-    dogs: [],
-    loading: true,
-  };
+function MainPage() {
+	const [dogs, setDogs] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    fetch("http://localhost:3500/animals")
-      .then((response) => response.json())
-      .then((data) =>
-        this.setState({
-          dogs: data,
-					loading:false
-        })
-      );
-  }
-
-  searchDogs = (queryStr, gender = "Male, Female") => {
-		this.setState({loading: true});
+  const searchDogs = (queryStr, gender = "Male, Female") => {
+		setLoading(true);
     fetch(`http://localhost:3500/animals?breeds_like=${queryStr}${gender !== "Male, Female" ? `&gender=${gender}` : ""}`)
       .then((response) => response.json())
-      .then((data) => this.setState({
-          dogs: data,
-					loading: false
-        })
-      );
+      .then((data) => {
+				setDogs(data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.error(err);
+				setLoading(false);
+			});
   };
 
-  render() {
-    const { dogs, loading } = this.state;
+	useEffect(() => {
+		fetch("http://localhost:3500/animals")
+		.then((response) => response.json())
+		.then((data) => {
+			setDogs(data);
+			setLoading(false);
+		})
+		.catch((err) => {
+			console.error(err);
+			setLoading(false);
+		});
+	}, [])
 
-    return (
-      <div className={MainPageStyle.mainPage}>
-        <Header />
-        <div className="content">
-          <Search searchDogs={this.searchDogs} />
-          <div className="main__body">
-            {loading ? (<Preloader />) : (<Dogs dogs={dogs} />)}
-          </div>
+  return (
+    <div className={MainPageStyle.mainPage}>
+      <Header />
+      <div className="content">
+        <Search searchDogs={searchDogs} />
+        <div className="main__body">
+          {loading ? (<Preloader />) : (<Dogs dogs={dogs} />)}
         </div>
-        <Footer />
       </div>
-    );
-  }
+      <Footer />
+    </div>
+  );
 }
 
 export { MainPage };
